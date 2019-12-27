1565395dad57be728f2f21ef8a81f15389ae504d
Peter Karth <falonofthetower@gmail.com>
2019-12-27 12:14:10 -0500

    Prevent seeds from running in test setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,23 @@
+if Rails.env.development?
+  payload = JSON.parse(File.read("#{Rails.root}/spec/fixtures/webhooks/single.json")).fetch("body").first
 
-payload = JSON.parse(File.read("#{Rails.root}/spec/fixtures/webhooks/single.json")).fetch("body").first
 
+  colors = %w(Blue Red Green Yellow Black)
+  types = %w(Dark Light Hazy Fresh)
 
-colors = %w(Blue Red Green Yellow Black)
-types = %w(Dark Light Hazy Fresh)
-
-colors.each do |color|
-  FactoryBot.create(:plan, name: color)
-end
-
-FactoryBot.create(:match, plan: Plan.find_by(name: 'Blue'), match_string: "Blue~Dark")
-
-3.times do
   colors.each do |color|
-    types.each do |type|
-      payload["Plan"]["Name"] = color
-      payload["Company"]["Name"] = type
-      FactoryBot.create(:event, json_payload: payload)
+    FactoryBot.create(:plan, name: color)
+  end
+
+  FactoryBot.create(:match, plan: Plan.find_by(name: 'Blue'), match_string: "Blue~Dark")
+
+  3.times do
+    colors.each do |color|
+      types.each do |type|
+        payload["Plan"]["Name"] = color
+        payload["Company"]["Name"] = type
+        FactoryBot.create(:event, json_payload: payload)
+      end
     end
   end
 end
